d4a0d8a823ed404ad5dbb30e3dcca13fb40fab46
HamedMiri72 <hamedmiri72@gmail.com>
2024-10-25 22:00:01 +0100

    category-service-imp

--- src/main/java/com/hamed/service/CategoryServiceImp.java
+++ src/main/java/com/hamed/service/CategoryServiceImp.java
@@ -1,2 +1,45 @@
-package com.hamed.service;public class CategoryServiceImp {
+package com.hamed.service;
+
+import com.hamed.model.Category;
+import com.hamed.model.Restaurant;
+import com.hamed.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImp implements CategoryService{
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @Override
+    public Category createCategory(String name, Long userId) throws Exception {
+        Restaurant restaurant = restaurantService.findResturantById(userId);
+        Category category = new Category();
+        category.setName(name);
+        category.setRestaurant(restaurant);
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> findCategoryByRestaurantId(Long id) throws Exception {
+        return categoryRepository.findByRestaurantId(id);
+
+    }
+
+    @Override
+    public Category findCategoryById(Long id) throws Exception {
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+
+        if(optionalCategory.isEmpty()){
+            throw new Exception("category is not found");
+        }
+        return optionalCategory.get();
+    }
 }
